297990fc1226cbdf71e3336bbed14d938871791c
Ali Ibrahim <36858894+AliIbrahim996@users.noreply.github.com>
2022-10-25 11:26:38 +0300

    Add docs for the VDP creator

--- VertexDisjointPath/include/algorithms/vertexDisjointPathCreator.h
+++ VertexDisjointPath/include/algorithms/vertexDisjointPathCreator.h
@@ -1,8 +1,6 @@
-//!
+//! @copyright 2022 All rights reserved.
+//! @author Ali Ibrahim
 //! 
-//! 
-//! 
-
 
 #ifndef VDP_VERTEX_DISJOINT_PATH_CREATOR_H
 #define VDP_VERTEX_DISJOINT_PATH_CREATOR_H
@@ -13,6 +11,24 @@ namespace VDP
 {
 	namespace Algorithm
 	{
+		//! Create the required flow network from the input flow network following these steps
+		//!  Split each node v in the graph into to nodes: vin and vout.
+		//! 
+		//!		1- For each node v, add an edge of capacity one from vin to vout.
+		//!		2- Replace each other edge(u, v) in the graph with an edge from uout to vin of capacity 1.
+		//!		3- Add in a new dedicated destination node t.
+		//! For each of the target nodes v, add an edge from vin to t with capacity 1.
+		//! Find a max - flow from sout to t.The value of the flow is the number of node - disjoint paths.
+		//! The idea behind this construction is as follows.Any flow path from the start node s to the destination node t must have capacity one, 
+		//! since all edges have capacity one.Since all capacities are integral, there exists an integral max - flow.
+		//! No two flow paths can pass through the same intermediary node, because in passing through a node in the graph the flow path must cross the edge from vin to vout, and the capacity here has been restricted to one.
+		//! Additionally, this flow path must arrive at t by ending at one of the three special nodes you've identified, then following the edge from that node to t.
+		//! Thus each flow path represents a node-disjoint path from the source node s to one of the three destination nodes.
+		//! Accordingly, computing a max-flow here corresponds to finding the maximum number of node-disjoint paths you can take from s to any of the three destinations.
+		//! @param[in] graph The flow network which will be transformed.
+		//! @param[in] src The source node.
+		//! @param[in] dest The destenation node.
+		//! @returns The corresponding flow notwork after replacing each other edge(u, v) in the graph with an edge from uout to vin of capacity 1.
 		std::unique_ptr<AdjListGraph> createVertexDisjointPathInput(const AdjListGraph& graph, int src, int dest) {
 			std::unique_ptr<AdjListGraph> listGraph = std::make_unique<AdjListGraph>(2 * graph.getSize() - 2);
 			int preSize = graph.getSize();
